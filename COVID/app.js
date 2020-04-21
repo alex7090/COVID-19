1c6cc776329872995535e0c7610c65736bc3ddfb
al3x7o9o <38883070+al3x7o9o@users.noreply.github.com>
2020-04-21 14:56:24 +0200

    BACK BASIC
    Bases du backend.
    2 options :  API et GitHub

---
+++ COVID/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const session = require('express-session');
+
+const app = express();
+
+app.use(express.static(__dirname + '/views'));
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs')
+
+
+
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+
+
+//Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
